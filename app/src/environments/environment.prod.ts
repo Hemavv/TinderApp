481a1f435766539a5a0f32e46a9c5ff573ed5868
Hema.V <hema.v@mtrix.co>
2018-07-30 10:45:04 +0530

    Initial commit

---
+++ app/src/environments/environment.prod.ts
@@ -0,0 +1,23 @@
+export const environment = {
+    "envId": "f1c15dd1-8640-3919-e8b0-3b7a78327874",
+    "name": "prod",
+    "properties": {
+        "production": true,
+        "baseUrl": "http://localhost:3000/bhive-art/",
+        "tenantName": "tindertenant",
+        "appName": "tinderapp",
+        "namespace": "com.neutrinos.tindertenant.tinderapp",
+        "isNotificationEnabled": false,
+        "googleMapKey": "AIzaSyCSTnVwijjv0CFRA4MEeS-H6PAQc87LEoU",
+        "firebaseSenderId": "FIREBASE_SENDER_ID",
+        "firebaseAuthKey": "FIREBASE_AUTH_KEY",
+        "authDomain": "FIREBASE_AUTH_DOMAIN",
+        "databaseURL": "FIREBASE_DATABASE_URL",
+        "storageBucket": "FIREBASE_STORAGE_BUCKET",
+        "messagingSenderId": "FIREBASE_SENDER_ID",
+        "appDataSource": "APP_DATA_SOURCE",
+        "appAuthenticationStrategy": "basicAuth",
+        "basicAuthUser": "username",
+        "basicAuthPassword": "password"
+    }
+}
---
+++ app/src/environments/environment.ts
@@ -0,0 +1,23 @@
+export const environment = {
+    "envId": "3c58466f-50e8-04e0-07d0-580098e2d1e3",
+    "name": "dev",
+    "properties": {
+        "production": false,
+        "baseUrl": "http://localhost:3000/bhive-art/",
+        "tenantName": "tindertenant",
+        "appName": "tinderapp",
+        "namespace": "com.neutrinos.tindertenant.tinderapp",
+        "isNotificationEnabled": false,
+        "googleMapKey": "AIzaSyCSTnVwijjv0CFRA4MEeS-H6PAQc87LEoU",
+        "firebaseSenderId": "FIREBASE_SENDER_ID",
+        "firebaseAuthKey": "FIREBASE_AUTH_KEY",
+        "authDomain": "FIREBASE_AUTH_DOMAIN",
+        "databaseURL": "FIREBASE_DATABASE_URL",
+        "storageBucket": "FIREBASE_STORAGE_BUCKET",
+        "messagingSenderId": "FIREBASE_SENDER_ID",
+        "appDataSource": "APP_DATA_SOURCE",
+        "appAuthenticationStrategy": "basicAuth",
+        "basicAuthUser": "username",
+        "basicAuthPassword": "password"
+    }
+}
